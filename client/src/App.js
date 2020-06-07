a5becce61a71109cd33ea0898a2e1f57ab6e4e77
Cletus Dias <cletus6@gmail.com>
2020-06-06 23:57:51 -0400

    Finished with Register & Login form

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+import './App.css';
+//  Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
+const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Route exact path='/' component={Landing}></Route>
+					<section className='container'>
+						<Alert />
+						<Switch>
+							<Route exact path='/register' component={Register}></Route>
+							<Route exact path='/login' component={Login}></Route>
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
+
+export default App;
--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
